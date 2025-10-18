c872ca06b003215bb926c03974981fb7974df5fc
GoregenB <goregenb2010@gmail.com>
2025-10-18 14:46:09 +0300

    Update script.js
    Replaced English messages with Turkish translations.

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Emin misin?",
+    "Gerçektenmi??",
+    "Pozitifmisin?",
+    "Abii lütfenn...",
+    "Sadece bir düşün!",
+    "Hayır dersen çok üzülürüm...",
+    "Çok üzüleceğim...",
+    "Çok çok çok üzgün olacağım...",
+    "Tamam tamam sormayı bırakacağım...",
+    "Şaka yapıyorum sadece evet de! ❤️"
 ];
 
 let messageIndex = 0;
